a00f20ee9411b4112f3b3e3fa744e236de76c0c5
QuantumSheep <quantumsheep@outlook.fr>
2019-08-21 15:34:47 +0200

    Add onClick event on DesktopIcon component

--- src/components/DesktopIcon.js
+++ src/components/DesktopIcon.js
@@ -5,13 +5,15 @@ import default_icon from '../ressources/icons/default.png'
 
 class DesktopIcon extends React.Component {
   static defaultProps = {
+    onClick: () => { },
+
     icon: default_icon,
     title: "???",
   }
 
   render() {
     return (
-      <div className="desktop-icon">
+      <div className="desktop-icon" onClick={this.props.onClick}>
         <div className="desktop-icon-image" style={{ backgroundImage: `url(${this.props.icon})` }} />
         <div className="desktop-icon-title">{this.props.title}</div>
       </div>
