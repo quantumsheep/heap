210e36266fc670bdc224240796d3f8930696f803
quantumsheep <nathanael.dmc@outlook.fr>
2019-08-21 22:24:51 +0200

    Rework windows creation + Add functionnal README

---
+++ src/files/README.md
@@ -0,0 +1,2 @@
+# HEAP
+HEAP is an online computer science RPG game where you fight other player by creating an AI and hacking their memory.
--- src/views/App.js
+++ src/views/App.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import axios from 'axios'
+import ReactMarkdown from 'react-markdown'
 
 import DesktopFiles from '../components/DesktopFiles'
 import DesktopIcon from '../components/DesktopIcon'
@@ -7,6 +9,8 @@ import HeapWindow from '../components/HeapWindow'
 
 import internet_icon from '../ressources/icons/internet.png'
 import textfile_icon from '../ressources/icons/textfile.png'
+
+import file_readme from '../files/README.md'
 
 class App extends React.Component {
   constructor(props) {
@@ -20,15 +24,15 @@ class App extends React.Component {
 
   /**
    * @param {string} title 
-   * @param {JSX.Element} body 
+   * @param {() => Promise<JSX.Element>} body 
    */
-  createWindow = (title, body) => {
+  createWindow = async (title, body) => {
     const windows = this.state.windows
     const key = 'window-' + windows.length
 
     windows.push((
       <HeapWindow title={title} key={key} dataKey={key} onClose={this.closeWindow}>
-        {body}
+        {await body()}
       </HeapWindow>
     ))
 
@@ -41,20 +45,30 @@ class App extends React.Component {
     })
   }
 
-  openInternet = () => this.createWindow('Internet', (
-    <div>Welcome, this is the readme file!</div>
-  ))
+  internet = async () => (
+    <div style={{ padding: '4px' }}>Welcome to the World Wide Web!</div>
+  )
+
+  readme = async () => {
+    const { data } = await axios.get(file_readme)
+
+    return (
+      <div style={{ padding: '4px' }}>
+        <ReactMarkdown source={data} escapeHtml={false} />
+      </div>
+    )
+  }
 
   render() {
     return (
       <main>
-        <DesktopFiles>
-          <DesktopIcon icon={internet_icon} title="Internet" onClick={this.openInternet} />
-          <DesktopIcon icon={textfile_icon} title="README" />
-        </DesktopFiles>
         <WindowManager>
           {this.state.windows}
         </WindowManager>
+        <DesktopFiles>
+          <DesktopIcon icon={internet_icon} title="Internet" onClick={() => this.createWindow("Internet", this.internet)} />
+          <DesktopIcon icon={textfile_icon} title="README" onClick={() => this.createWindow("README", this.readme)} />
+        </DesktopFiles>
       </main>
     )
   }
