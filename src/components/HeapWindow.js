a2f20faf4394cf916f98516b1d527fd6ed5b5184
QuantumSheep <quantumsheep@outlook.fr>
2019-08-22 13:56:44 +0200

    Add possibility to resize windows

--- src/components/HeapWindow.js
+++ src/components/HeapWindow.js
@@ -2,6 +2,7 @@ import React from 'react'
 import '../style/heap-window.css'
 
 import Draggable from 'react-draggable'
+import { Resizable } from 're-resizable'
 
 import CloseButton from './CloseButton'
 
@@ -20,7 +21,8 @@ class HeapWindow extends React.Component {
   constructor(props) {
     super(props)
 
-    this.reference = React.createRef();
+    /** @type {Resizable} */
+    this.reference = null;
 
     this.props.incrementIndex()
 
@@ -38,8 +40,8 @@ class HeapWindow extends React.Component {
 
     this.setState({
       position: {
-        x: (document.body.clientWidth / 2) - (this.reference.current.clientWidth / 2) + (rand(-OFFSET, OFFSET)),
-        y: (document.body.clientHeight / 2) - (this.reference.current.clientHeight / 2) + (rand(-OFFSET, OFFSET)),
+        x: (document.body.clientWidth / 2) - (this.reference.resizable.clientWidth / 2) + (rand(-OFFSET, OFFSET)),
+        y: (document.body.clientHeight / 2) - (this.reference.resizable.clientHeight / 2) + (rand(-OFFSET, OFFSET)),
       },
     })
   }
@@ -50,6 +52,49 @@ class HeapWindow extends React.Component {
   }
 
   onDrag = (e, position) => {
+    this.setState({ position })
+  }
+
+  is_percantage = str => typeof (str) === 'string' && str.endsWith('%')
+  get_size_from_percentage = (percantage, parent_size) => parent_size * +percantage.slice(0, -1) / 100
+
+  /**
+   * @param {MouseEvent | TouchEvent} e 
+   * @param {import('re-resizable').ResizeDirection} dir 
+   * @param {HTMLDivElement} ref 
+   */
+  onResize = (e, dir, ref) => {
+    console.log(dir)
+    if (dir !== "topLeft" && dir !== "top" && dir !== "left" && dir !== "bottomLeft" && dir !== "topRight") return
+
+    const position = this.state.position
+
+    let { minHeight, maxHeight, minWidth, maxWidth } = this.reference.props
+
+    if (this.is_percantage(minWidth)) {
+      minWidth = this.get_size_from_percentage(minWidth, ref.parentElement.clientWidth)
+    }
+
+    if (this.is_percantage(maxWidth)) {
+      maxWidth = this.get_size_from_percentage(maxWidth, ref.parentElement.clientWidth)
+    }
+
+    if (this.is_percantage(minHeight)) {
+      minHeight = this.get_size_from_percentage(minHeight, ref.parentElement.clientHeight)
+    }
+
+    if (this.is_percantage(maxHeight)) {
+      maxHeight = this.get_size_from_percentage(maxHeight, ref.parentElement.clientHeight)
+    }
+
+    if ((dir === "topLeft" || dir === "top" || dir === "topRight") && (minHeight === undefined || minHeight <= ref.clientHeight) && (maxHeight === undefined || maxHeight >= ref.clientHeight)) {
+      position.y = e.clientY
+    }
+
+    if ((dir === "topLeft" || dir === "left" || dir === "bottomLeft") && (minWidth === undefined || minWidth <= ref.clientWidth) && (maxWidth === undefined || maxWidth >= ref.clientWidth)) {
+      position.x = e.clientX
+    }
+
     this.setState({ position })
   }
 
@@ -72,7 +117,19 @@ class HeapWindow extends React.Component {
         position={this.state.position}
         onDrag={this.onDrag}
       >
-        <div ref={this.reference} className="heap-window" style={{ zIndex: this.state.zindex }}>
+        <Resizable
+          ref={c => this.reference = c}
+          className="heap-window"
+          style={{ zIndex: this.state.zindex }}
+          onResize={this.onResize}
+          defaultSize={{
+            width: 500,
+            height: 750,
+          }}
+          minWidth={350}
+          minHeight={200}
+          maxWidth="100%"
+        >
           <div className="heap-window-title-bar">
             <div className="heap-window-title-title">{this.props.title}</div>
             <div className="heap-window-title-buttons">
@@ -80,8 +137,8 @@ class HeapWindow extends React.Component {
             </div>
           </div>
           <div onMouseDown={this.setOnTop}>{this.props.children}</div>
-        </div>
-      </Draggable>
+        </Resizable>
+      </Draggable >
     )
   }
 }
