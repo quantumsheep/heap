11a23a4bc804ed8e006a4958537b71ca8a5c2680
quantumsheep <nathanael.dmc@outlook.fr>
2019-08-25 18:49:41 +0200

    Add normalize.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import 'normalize.css'
+
 import App from './views/App'
 import * as serviceWorker from './serviceWorker'
 
